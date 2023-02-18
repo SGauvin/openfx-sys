f349820ce6e9b077c639be1ab9897380400669cb
Simon Gauvin <gauvinsimon@gmail.com>
2023-02-17 21:03:03 -0500

    Add bindings

---
+++ wrapper.h
@@ -0,0 +1,21 @@
+#if !defined(__cplusplus)
+#include <stdbool.h>
+#endif
+
+#include "openfx/include/ofxCore.h"
+#include "openfx/include/ofxDialog.h"
+#include "openfx/include/ofxDrawSuite.h"
+#include "openfx/include/ofxImageEffect.h"
+#include "openfx/include/ofxInteract.h"
+#include "openfx/include/ofxKeySyms.h"
+#include "openfx/include/ofxMemory.h"
+#include "openfx/include/ofxMessage.h"
+#include "openfx/include/ofxMultiThread.h"
+#include "openfx/include/ofxOld.h"
+#include "openfx/include/ofxOpenGLRender.h"
+#include "openfx/include/ofxParam.h"
+#include "openfx/include/ofxParametricParam.h"
+#include "openfx/include/ofxPixels.h"
+#include "openfx/include/ofxProgress.h"
+#include "openfx/include/ofxProperty.h"
+#include "openfx/include/ofxTimeLine.h"
